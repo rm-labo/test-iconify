25d11f5885b2090eaa577da40a137b4544ff4169
rm <mizukami@rm-labo.com>
2022-03-03 11:58:05 +0900

    add: nuxt(v2)

---
+++ nuxt-iconify-test/components/IconBase.vue
@@ -0,0 +1,20 @@
+<template>
+  <Icon class="IconBase" :icon="iconName" />
+</template>
+
+<script>
+import { Icon } from '@iconify/vue2';
+export default {
+  name: 'IconBase',
+  components: {
+    Icon
+  },
+  props: {
+    iconName: {
+      type: String,
+      default: '',
+      required: true,
+    }
+  }
+}
+</script>
---
+++ nuxt-iconify-test/pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div style="font-size: 2rem;">
+    <h1>iconify test nuxt(v2)</h1>
+    <div>
+      <IconBase icon-name="ep:circle-close" />
+      <IconBase icon-name="ep:aim" />
+      <IconBase icon-name="ep:alarm-clock" />
+      <IconBase icon-name="ep:apple" />
+      <IconBase icon-name="ep:arrow-down" />
+      <IconBase icon-name="ep:avatar" />
+      <IconBase icon-name="ep:baseball" />
+      <IconBase icon-name="ep:bicycle" />
+      <IconBase icon-name="ep:box" />
+      <IconBase icon-name="ep:chat-dot-square" />
+    </div>
+    <div>
+      <IconBase icon-name="fa6-solid:pager" />
+      <IconBase icon-name="fa6-solid:paint-roller" />
+      <IconBase icon-name="fa6-solid:paintbrush" />
+      <IconBase icon-name="fa6-solid:palette" />
+      <IconBase icon-name="fa6-solid:paperclip" />
+      <IconBase icon-name="fa6-solid:pen-fancy" />
+      <IconBase icon-name="fa6-solid:people-arrows-left-right" />
+      <IconBase icon-name="fa6-solid:person-booth" />
+      <IconBase icon-name="fa6-solid:person-skating" />
+      <IconBase icon-name="fa6-solid:object-ungroup" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import IconBase from '../components/IconBase.vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    IconBase
+  },
+})
+</script>
